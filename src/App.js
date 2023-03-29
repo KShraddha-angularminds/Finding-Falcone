4f2f89cee1ab30815b37d57aa1d4db7819a99b60
KShraddha-angularminds <shraddha.karpe@angularminds.in>
2023-03-29 15:13:23 +0530

    tasks

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">hello everyone and welcome</div>;
+  return <div className="App">hello everyone okk welcome</div>;
 }
 
 export default App;
