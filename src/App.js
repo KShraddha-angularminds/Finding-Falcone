4287c223075f2291781093b785c1ab01b761a1e2
KShraddha-angularminds <shraddha.karpe@angularminds.in>
2023-03-29 17:50:27 +0530

    Revert "Merge branch 'tasks' into main"
    This reverts commit 107e1be261e643041a466112227871dab29c1396, reversing
    changes made to 61951e875c16b39623ea6abb8911398e29e0d3be.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ function App() {
   return (
     <div>
       <div className="App">hello 1</div>
+      <div className="App">2</div>
+      <div className="App">678</div>
+      <div className="App">4</div>
+      <div className="App">5</div>
     </div>
   );
 }
