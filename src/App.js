3277f1fb852322435b0efbf9d253c356d5c56ef2
KShraddha-angularminds <shraddha.karpe@angularminds.in>
2023-03-29 14:44:23 +0530

    Revert "new changes"
    This reverts commit afa413b35add46cc27077842dd72be7bc5ba7f8a.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">hello everyone fhfhhjhfdhj</div>;
+  return <div className="App">hello everyone</div>;
 }
 
 export default App;
