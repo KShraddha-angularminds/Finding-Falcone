7fef3fbcbdf4c5768e53a5d79adcfaa5d5dd6fb1
KShraddha-angularminds <shraddha.karpe@angularminds.in>
2023-03-29 16:09:54 +0530

    main remove welcome

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">hello everyone welcome to Falcon</div>;
+  return <div className="App">hello everyone to Falcon</div>;
 }
 
 export default App;
